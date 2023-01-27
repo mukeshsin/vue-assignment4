580dc11d0d62cf737cc63ab7d187210c6910f1f1
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-27 19:10:58 +0530

    worked on signup form

---
+++ vue-app/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+<div class="form-body">
+    <signup_form />
+
+</div>
+</template>
+
+<script>
+import signup_form from './components/SignupForm.vue'
+
+export default {
+    name: 'App',
+
+    data() {
+        return {
+            email: "",
+            password: ""
+        }
+
+    },
+    components: {
+        signup_form
+
+    }
+}
+</script>
+
+<style>
+.form-body {
+    width: 500px;
+    height: 850px;
+    background-color: aqua;
+    border-radius: 10px;
+    align-self: center;
+
+}
+</style>
---
+++ vue-app/src/components/SignupForm.vue
@@ -0,0 +1,24 @@
+<template>
+<div class="formstart">
+    <label class="formlabel"></label>
+    <input type="email" v-model="email" />
+
+    <label class="formlabel"></label>
+    <input type="password" v-model="password" />
+
+</div>
+</template>
+
+<script>
+export default {
+    name: 'signup-form',
+    data() {
+        return {}
+    },
+
+}
+</script>
+
+<style>
+
+</style>
