d2e3fe1b10f296aec607cf0920c858829d0815ba
mukeshsin <110765718+mukeshsin@users.noreply.github.com>
2023-01-29 00:46:29 +0530

    completed signup form

--- vue-app/src/App.vue
+++ vue-app/src/App.vue
@@ -1,37 +1,35 @@
 <template>
 <div class="form-body">
     <signup_form />
-
 </div>
 </template>
 
 <script>
-import signup_form from './components/SignupForm.vue'
+import signup_form from "./components/SignupForm.vue";
 
 export default {
-    name: 'App',
+    name: "App",
 
     data() {
-        return {
-            email: "",
-            password: ""
-        }
-
+        return {};
     },
     components: {
-        signup_form
-
-    }
-}
+        signup_form,
+    },
+};
 </script>
 
 <style>
 .form-body {
-    width: 500px;
-    height: 850px;
-    background-color: aqua;
+    height: 100%;
+    width: 50%;
+    margin-left: 30px;
+    padding-left: 30px;
+    background-color: white;
+    margin-top: 1.5%;
     border-radius: 10px;
-    align-self: center;
-
+    color: #bec2c4;
+    margin-bottom: 30px;
+    border: 15px #ededed solid;
 }
 </style>
